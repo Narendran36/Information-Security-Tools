96b246c3b545084489fe41385e17aa49ac58afbb
Narendran S Nair <unninarendran36@gmail.com>
2020-07-04 16:18:35 +0530

    Create Heuristic_Frequency_Analysis.py

---
+++ Heuristic_Frequency_Analysis.py
@@ -0,0 +1,26 @@
+from collections import Counter
+
+def is_ascii(s):
+    return all(ord(c) < 128 for c in s)
+
+def is_english(s):
+    pass # return boolean
+    frequent = ['e', 't', 'a', 'o', 'i', 'n']
+    if is_ascii(s):
+        d = {}
+        top3 = []
+        s = s.lower()
+        for ch in s:
+            if ch.isalpha():
+                if ch in d:
+                    d[ch] = d[ch] + 1
+                else:
+                    d[ch] = 1
+        d_order = sorted(d.items(),key=lambda x: x[1],reverse=True)[:3]
+        print(d_order)
+        if d_order == []:
+            return False
+        for x in d_order:
+            if x[0] not in frequent:
+                return False
+        return True
