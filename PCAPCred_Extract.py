1dccee852e2227aff1f2efc7f0c2fd51b0995e25
Narendran S Nair <unninarendran36@gmail.com>
2020-09-08 23:08:01 +0530

    Create PCAPCred_Extract.py

---
+++ PCAPCred_Extract.py
@@ -0,0 +1,22 @@
+from scapy.all import *
+from scapy.layers.http import *
+import sys # ignore
+sys.path.insert(0,'.') # ignore
+from create_recording import recording_path # the path to the pcap file of this assignment
+import re
+
+#### Don't change the code until this line ####
+
+def show_username_password():
+    bind_layers( TCP, HTTP, sport=8000 )
+    bind_layers( TCP, HTTP, dport=8000 )
+    packets = rdpcap(recording_path)
+    pattern = 'username=(.*)&password=([^\s]+)'
+    for packet in packets:
+        pattern = 'username=(.*)&password=([^\s]+)'
+        content = raw(packet[HTTP]).decode('ascii')
+        match = re.search(pattern,content)
+        if match:
+            print(match.group(0))
+        
+show_username_password()
