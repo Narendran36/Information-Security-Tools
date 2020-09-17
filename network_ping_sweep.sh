52f94de9307f0262c1333a2de20153bba9e2145a
Narendran S Nair <unninarendran36@gmail.com>
2020-09-17 11:09:19 +0530

    Create network_ping_sweep.sh

---
+++ network_ping_sweep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$1" == "" ]
+then 
+echo "Usage: ./ping.sh [network]"
+echo "Example: ./ping.sh 192.168.1"
+else
+for x in `seq 1 254`; do
+ping -c 1 $1.$x | grep "64 bytes" | cut -d " " -f 4 | sed 's/.$//'
+done
+fi
